98cddd7f1cff36936701de0074d10fad9b58a312
reha54321 <reha.mathur@gmail.com>
2020-06-06 01:56:30 -0400

    added rich text support to updated code

--- devconnect/home/routes.py
+++ devconnect/home/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Markup
 
 from devconnect.home import bp
 from devconnect.models import Post
@@ -10,4 +10,4 @@ def index():
     page = request.args.get('page',1, type=int)
     pagination = Post.query.order_by('created').paginate(page, per_page=10, error_out=False)
     posts = pagination.items
-    return render_template('index.html', posts=posts, pagination=pagination)
+    return render_template('index.html', posts=posts, pagination=pagination, Markup=Markup)
